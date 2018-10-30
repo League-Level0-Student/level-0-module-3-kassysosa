b0a0326be4b74abc5e61403c03a819b77ea1c20a
350969 <350969@0338S-S-R0B6ZH2.sdcs.local>
2018-10-30 14:22:15 -0700

    Horoscope

---
+++ src/extra/Horoscopes.java
@@ -0,0 +1,10 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Horoscopes {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("What is your star sign?");
+	if(answer.)
+}
+}
