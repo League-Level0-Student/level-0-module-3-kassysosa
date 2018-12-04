d85d6f6239980983f6a84f40ad27ba6c5cf22f21
kassysosa <37815881+kassysosa@users.noreply.github.com>
2018-12-04 14:31:22 -0800

    Add files via upload

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,20 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String answer = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int value = Integer.parseInt(answer);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if(value >= 3) { JOptionPane.showMessageDialog(null, "You are a crazy cat lady!");
+	
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+else if(value<3 && value > 0){
+	playVideo("https://youtu.be/4-0zb1LtXZg");
+	}
+}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
-	}
-
+	
+	
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
